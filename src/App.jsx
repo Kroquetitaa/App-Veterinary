99e73189099044673ce565f84af2917d9bb71828
Kroquetitaa <Kroquetita@gmail.com>
2022-10-01 18:57:13 +0200

    add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
@@ -7,22 +7,35 @@ const App = () => {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
-  const eliminarPaciente = id => {
-    const pacientesActualizados = pacientes.filter( paciente => paciente.id !== id);
+  useEffect(() => {
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(pacientesLS);
+    }
+    obtenerLS();
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(
+      (paciente) => paciente.id !== id
+    );
     setPacientes(pacientesActualizados);
-  }
+  };
 
   return (
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 flex">
-        <Formulario 
-         pacientes={pacientes}
+        <Formulario
+          pacientes={pacientes}
           setPacientes={setPacientes}
           paciente={paciente}
         />
-        <ListadoPacientes 
+        <ListadoPacientes
           pacientes={pacientes}
           setPaciente={setPaciente}
           eliminarPaciente={eliminarPaciente}
